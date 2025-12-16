3f489411d651f31a042c2d2e634a4dd757271610
praveen1535 <praveenbandi153@gmail.com>
2025-12-16 08:00:57 +0530

    Message has been modified

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -2,13 +2,17 @@ public class StringDemo {
     public static void main(String[] args) {
 
         String str1 = new String("Good Morning Praveen!");
-        String str2 = new String("Good Morning Akshaya!");
 
         System.out.println(str1);
+<<<<<<< HEAD
         System.out.println(str2);
 
         System.out.println("Length of str1: " + str1.length());
         System.out.println("Length of str2: " + str2.length());
 
+=======
+
+        System.out.println("Length of str1: " + str1.length());
+>>>>>>> 30cbdd4 (Message has been modified)
     }
 }
