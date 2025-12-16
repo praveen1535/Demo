51982ef47fb605e06d5189d41e53117877da3659
praveen1535 <praveenbandi153@gmail.com>
2025-12-16 08:14:44 +0530

    Updated info

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -4,15 +4,7 @@ public class StringDemo {
         String str1 = new String("Good Morning Praveen!");
 
         System.out.println(str1);
-<<<<<<< HEAD
-        System.out.println(str2);
 
         System.out.println("Length of str1: " + str1.length());
-        System.out.println("Length of str2: " + str2.length());
-
-=======
-
-        System.out.println("Length of str1: " + str1.length());
->>>>>>> 30cbdd4 (Message has been modified)
     }
 }
