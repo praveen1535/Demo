5889fcb8b508340b82b9b9688d6ec3b5d9a046f8
praveen1535 <praveenbandi153@gmail.com>
2025-12-16 07:49:58 +0530

    Project has been modified

--- src/StringDemo.java
+++ src/StringDemo.java
@@ -2,9 +2,16 @@ public class StringDemo {
     public static void main(String[] args) {
 
         String str1 = new String("Good Morning Praveen!");
-        String str2 = new String("Good Morning Chandana!");
+        String str2 = new String("Good Morning Akshaya!");
 
         System.out.println(str1);
         System.out.println(str2);
+<<<<<<< HEAD
+=======
+
+        System.out.println("Length of str1: " + str1.length());
+        System.out.println("Length of str2: " + str2.length());
+
+>>>>>>> 9719b0e (Project has been modified)
     }
 }
